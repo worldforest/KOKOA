1efb9477626046f65f9515063c15f0b0006e47cb
diatkdwls <diatkdwls@naver.com>
2020-10-28 16:23:04 +0900

    api test

--- testapi/src/main/java/controller/RomaController.java
+++ testapi/src/main/java/controller/RomaController.java
@@ -1,5 +1,22 @@
 package controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+
+@ApiResponses(value = {
+	      @ApiResponse(code = 401, message = "Unauthorized"),
+	      @ApiResponse(code = 403, message = "Forbidden"),
+	      @ApiResponse(code = 404, message = "Not Found"),
+	      @ApiResponse(code = 500, message = "Failure"),
+	})
+@CrossOrigin(origins = { "*" })
+@RestController()
+@RequestMapping("/roma")
 public class RomaController {
 
 }
