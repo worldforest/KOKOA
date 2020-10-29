7f2e6f2f783d581de774667b88e9786ee3db9911
hjoo <hwandong.joo@gmail.com>
2020-10-29 16:07:11 +0900

    받아쓰기 데이터 추가

--- frontend/src/components/training/Write.vue
+++ frontend/src/components/training/Write.vue
@@ -2,6 +2,12 @@
   <div>
     <h1>받아쓰기 페이지</h1>
     <h2>name: {{ $route.params.index }}</h2>
+    <iframe v-bind="player"></iframe>
+    <div>
+      <b-button v-for="(item, index) in video" :key="index" @click="play(index)">{{
+        item.eng
+      }}</b-button>
+    </div>
   </div>
 </template>
 <script>
@@ -13,9 +19,43 @@ export default {
       default: '',
     },
   },
+  created() {
+
+  },
   mounted() {},
   data() {
-    return {};
+    return {
+      player: '',
+      url: 'https://www.youtube.com/embed/mLx7D98zP_A',
+      origin: '?enablejsapi=1&origin=*',
+      video: [
+        {
+          starttime: '00:00:00,000',
+          endtime: '00:00:01,831',
+          eng: '[National treasure zombie beast 2PM]',
+          kor: '[국보급 짐승 좀비 2PM]',
+        },
+        {
+          starttime: '00:00:01,832',
+          endtime: '00:00:04,034',
+          eng: '"Chansung lifts people up"',
+          kor: '"찬성은 사람을 들어"',
+        },
+        {
+          starttime: '00:00:04,035',
+          endtime: '00:00:06,406',
+          eng: '"Taecyeon is mad"',
+          kor: '"화가 잔뜩 난 택연"',
+        },
+      ],
+    };
+  },
+  methods: {
+    play(index) {
+      console.log(index);
+      console.log(this.player);
+      // player.seekTo(00:00:00,000,true);
+    },
   },
 };
 </script>
