4084c77aab9d3d3c75c88a29f1fb0de6d9910f62
hjoo <hwandong.joo@gmail.com>
2020-10-29 17:15:50 +0900

    add 무한반복기능

--- frontend/src/components/training/Write.vue
+++ frontend/src/components/training/Write.vue
@@ -2,7 +2,8 @@
   <div>
     <h1>받아쓰기 페이지</h1>
     <h2>name: {{ $route.params.index }}</h2>
-    <iframe v-bind="player"></iframe>
+    <youtube :video-id="url" ref="youtube" :player-vars="playerVars" @ended="onEnded"></youtube>
+    <button @click="playVideo">play</button>
     <div>
       <b-button v-for="(item, index) in video" :key="index" @click="play(index)">{{
         item.eng
@@ -19,15 +20,13 @@ export default {
       default: '',
     },
   },
-  created() {
-
-  },
+  created() {},
   mounted() {},
   data() {
     return {
-      player: '',
-      url: 'https://www.youtube.com/embed/mLx7D98zP_A',
+      url: 'mLx7D98zP_A',
       origin: '?enablejsapi=1&origin=*',
+      replay: '',
       video: [
         {
           starttime: '00:00:00,000',
@@ -48,13 +47,42 @@ export default {
           kor: '"화가 잔뜩 난 택연"',
         },
       ],
+      playerVars: {
+        autoplay: 1,
+        controls: 0,
+        loop: 1,
+      },
     };
   },
+  computed: {
+    player() {
+      return this.$refs.youtube.player;
+    },
+  },
   methods: {
-    play(index) {
-      console.log(index);
-      console.log(this.player);
-      // player.seekTo(00:00:00,000,true);
+    async play(index) {
+      const start = this.timer(this.video[index].starttime);
+      const end = this.timer(this.video[index].endtime);
+      this.replay = index;
+      await this.player.loadVideoById({
+        videoId: this.url,
+        startSeconds: start,
+        endSeconds: end,
+        suggestedQuality: 'default',
+      });
+    },
+    async playVideo() {
+      await this.player.playVideo();
+    },
+    timer(input) {
+      const hms = input;
+      const a = hms.split(':');
+      const s = a[2].split(',');
+      const ms = Number(a[0] * 60 * 60) + Number(a[1] * 60) + Number(s[0]) + Number(s[1] / 1000);
+      return ms;
+    },
+    onEnded() {
+      this.play(this.replay);
     },
   },
 };
