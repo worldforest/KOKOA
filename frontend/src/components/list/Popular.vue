cf051dbd11990da21592f597bf958d3ccbbabf18
hjoo <hwandong.joo@gmail.com>
2020-11-02 19:02:06 +0900

    .S03P31B104-82 시작 메인화면

--- frontend/src/components/list/Popular.vue
+++ frontend/src/components/list/Popular.vue
@@ -4,7 +4,18 @@
       <b-row>
         <b-col sm="12" class="overflow-hidden">
           <div class="d-flex align-items-center justify-content-between">
-            <h4 class="main-title">인기별</h4>
+            <h4 class="main-title">Your K-Pop Star</h4>
+          </div>
+          <VueSlickCarousel class="choice" v-bind="settings2">
+            <img
+              v-for="(star, index) in stars"
+              :src="require(`@/assets/`+star.group)"
+              :key="index"
+              class="pa-1"
+            />
+          </VueSlickCarousel>
+          <div class="d-flex align-items-center justify-content-between">
+            <h4 class="main-title">Favorite</h4>
           </div>
           <div class="upcoming-contens ma-5">
             <VueSlickCarousel v-bind="settings">
@@ -13,11 +24,12 @@
                 :src="item.url"
                 :key="index"
                 @click="showModal(index)"
+                class="pa-1"
               />
               <b-modal v-model="modalShow" centered>
                 <p class="my-4">{{ selected }}</p>
-                <b-button @click="goWrite">받아쓰기</b-button>
-                <b-button @click="goTalk">말하기</b-button>
+                <b-button @click="goWrite">Writing</b-button>
+                <b-button @click="goTalk">Speaking</b-button>
               </b-modal>
             </VueSlickCarousel>
           </div>
@@ -58,13 +70,67 @@ export default {
           url: 'http://img.youtube.com/vi/WotZgTcbHRA/0.jpg',
         },
       ],
+      stars: [
+        {
+          group: 'iu.jpg',
+          member: {
+            0: 'iu.jpg',
+          },
+        },
+        {
+          group: 'redvelvet.jpg',
+          member: {
+            0: 'irene.jpg',
+            1: 'seulki.jpg',
+            2: 'wendy.jpg',
+            3: 'joy.jpg',
+            4: 'yeri.jpg',
+          },
+        },
+        {
+          group: 'iu.jpg',
+          member: {
+            0: 'iu.jpg',
+          },
+        },
+        {
+          group: 'redvelvet.jpg',
+          member: {
+            0: 'irene.jpg',
+            1: 'seulki.jpg',
+            2: 'wendy.jpg',
+            3: 'joy.jpg',
+            4: 'yeri.jpg',
+          },
+        },
+        {
+          group: 'redvelvet.jpg',
+          member: {
+            0: 'irene.jpg',
+            1: 'seulki.jpg',
+            2: 'wendy.jpg',
+            3: 'joy.jpg',
+            4: 'yeri.jpg',
+          },
+        },
+      ],
       settings: {
         arrows: true,
         infinite: true,
         // focusOnSelect: true,
-        speed: 2000,
+        speed: 3000,
         slidesToShow: 3,
         slidesToScroll: 3,
+        rows: 2,
+      },
+      settings2: {
+        arrows: true,
+        infinite: true,
+        // variableWidth: true,
+        adaptiveHeight: true,
+        speed: 3000,
+        slidesToShow: 10,
+        slidesToScroll: 10,
       },
       selected: '',
       modalShow: false,
@@ -81,6 +147,10 @@ export default {
     goTalk() {
       this.$router.push({ name: 'Talk', params: { index: String(this.selected) } });
     },
+    // getImg(index) {
+    //   const img = this.stars[index];
+    //   return `../assets/${img.group}`;
+    // },
   },
 };
 </script>
@@ -92,5 +162,8 @@ export default {
 .slick-next:before {
   color: red !important;
   background-color: #eee;
+}
+.choice img {
+   height:100%;
 }
 </style>
