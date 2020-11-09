09e25b5925fe8ca397d8b77f03e9e71991335422
hyeongseoblim <iii3@cnu.ac.kr>
2020-11-09 18:03:04 +0900

    .S03P31B104-118 .S03P31B104-112 .S03P31B104-113 log rest api, note rest api 개발

--- backend/src/main/java/com/web/kokoa/model/log.java
+++ backend/src/main/java/com/web/kokoa/model/log.java
@@ -7,18 +7,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity
-@Getter
-@Setter
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-
-public class note {
+@Entity @Getter @Setter @Data
+@AllArgsConstructor @NoArgsConstructor
+public class log {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private int userid;
-    private int subtitleid;
-    private int type;
+    private int videoid;
 }
