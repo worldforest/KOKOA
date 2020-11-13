552cbd21444f3e0140a633c83ad65cea69fcd7fb
hyeongseoblim <iii3@cnu.ac.kr>
2020-11-13 13:51:06 +0900

    중복 추가 안되게 수정

--- backend/src/main/java/com/web/kokoa/controller/NoteController.java
+++ backend/src/main/java/com/web/kokoa/controller/NoteController.java
@@ -48,26 +48,33 @@ public class NoteController {
     @PostMapping("/insert")
     @ApiOperation(value = "오답노트 추가", notes = "user num에 맞는 video, subtitle 오답노트 추가.")
     public ResponseEntity<String> addtoNote(@RequestParam String email, @RequestParam int videoid, @RequestParam int subtitleid, @RequestParam int type, @RequestParam int engsubtitleid){
+
         int userid = userRepo.findUserByEmail(email).getId();
         String result = "";
-        if(type==0){
-            speechnote note = new speechnote();
-            note.setUserid(userid);
-            note.setSubtitleid(subtitleid);
-            note.setEngsubtitleid(engsubtitleid);
-            note.setVideoid(videoid);
-            speechNoteRepo.save(note);
-            result = "add to speechnote complete";
-        }else{
-            writenote note = new writenote();
-            note.setUserid(userid);
-            note.setSubtitleid(subtitleid);
-            note.setEngsubtitleid(engsubtitleid);
-            note.setVideoid(videoid);
-            writeNoteRepo.save(note);
-            result = "add to writenote complete";
+        if(subtitlesRepo.getAllById(subtitleid)!=null){
+            result = "It's Already Registerd";
+            return new ResponseEntity<String>(result,HttpStatus.OK);
         }
-        return new ResponseEntity<String>(result, HttpStatus.OK);
+        else {
+            if (type == 0) {
+                speechnote note = new speechnote();
+                note.setUserid(userid);
+                note.setSubtitleid(subtitleid);
+                note.setEngsubtitleid(engsubtitleid);
+                note.setVideoid(videoid);
+                speechNoteRepo.save(note);
+                result = "add to speechnote complete";
+            } else {
+                writenote note = new writenote();
+                note.setUserid(userid);
+                note.setSubtitleid(subtitleid);
+                note.setEngsubtitleid(engsubtitleid);
+                note.setVideoid(videoid);
+                writeNoteRepo.save(note);
+                result = "add to writenote complete";
+            }
+            return new ResponseEntity<String>(result, HttpStatus.OK);
+        }
     }
 
     @GetMapping("/load")
--- backend/src/main/java/com/web/kokoa/repository/SubtitlesRepo.java
+++ backend/src/main/java/com/web/kokoa/repository/SubtitlesRepo.java
@@ -8,14 +8,15 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface SubtitlesRepo extends JpaRepository<subtitles,String> {
+public interface SubtitlesRepo extends JpaRepository<subtitles, String> {
 
-    @Query(value = "select id, videoid, starttime, endtime, content, type from subtitles where videoid =:videoid order by id",nativeQuery = true)
+    @Query(value = "select id, videoid, starttime, endtime, content, type from subtitles where videoid =:videoid order by id", nativeQuery = true)
     List<subtitles> getAllByVideoid(int videoid);
-    @Query(value = "select content from subtitles where id =:subtitleid",nativeQuery = true)
+
+    @Query(value = "select content from subtitles where id =:subtitleid", nativeQuery = true)
     String getContentById(int subtitleid);
 
-    @Query(value = "select id, videoid, starttime, endtime, content, type from subtitles where id =:subtitleid",nativeQuery = true)
+    @Query(value = "select id, videoid, starttime, endtime, content, type from subtitles where id =:subtitleid", nativeQuery = true)
     subtitles getAllById(int subtitleid);
 
 }
