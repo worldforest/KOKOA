123384ab8b65e4ce2afcfc39b277a76a01a9de0a
hyeongseoblim <iii3@cnu.ac.kr>
2020-11-17 09:49:14 +0900

    .S03P31B104-127 .S03P31B104-126 note 삭제 및 페이징 처리

--- backend/src/main/java/com/web/kokoa/repository/VideoRepo.java
+++ backend/src/main/java/com/web/kokoa/repository/VideoRepo.java
@@ -16,6 +16,5 @@ public interface VideoRepo extends JpaRepository<video, String> {
 
         @Query(value="select id, groupid, title, url from video where id in (select videoid from video_member where memberid =:id) ",nativeQuery = true)
         Page<video> findVideosByMemberid(Pageable pageable, int id);
-        
         video findById(int id);
 }
